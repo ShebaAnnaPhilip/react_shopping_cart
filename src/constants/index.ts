2daacd82f2ce26a4b69d6602f1b5a41ef5dbabd8
Sheba <sheba.aphilip@gmail.com>
2020-02-16 15:43:23 +1300

    modified constants, included id for the products

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,21 +1,26 @@
 export const products = [
   {
+    id: "1",
     name: "Plumpy Nut",
     price: 25.76
   },
   {
+    id: "2",
     name: "Measles Vaccine",
     price: 90.51
   },
   {
+    id: "3",
     name: "Therapeutic Milk",
     price: 56.14
   },
   {
+    id: "4",
     name: "Pencils",
     price: 13.9
   },
   {
+    id: "5",
     name: "School Bags",
     price: 19.45
   }
