b4b40cfc4f5d4e519a66f396a8a5ccc22fa28492
Sheba <sheba.aphilip@gmail.com>
2020-02-16 17:51:10 +1300

    added function to display cart on click of cart icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.scss'
 import Cart from '../Cart/Cart';
 
 
 const Header =() =>{
+    const [isCartOpen, toggleCart] = useState(false);
+    const handleClickCart = () => toggleCart(!isCartOpen);
     return (
         <div className="container">
             <img src='/assets/logo.png' className="logo" alt="Logo" />
             <div className="cart">
                 <a  href="#">
-                    <img className="icon" src='/assets/carticon.png' alt="Cart" />
+                    <img className="icon" src='/assets/carticon.png' alt="Cart" onClick={handleClickCart}/>
                 </a>
-                <div className="cart-status">
+                <div className={isCartOpen?"cart-status open":"cart-status"}>
                     <Cart/>
                 </div>
             </div>
