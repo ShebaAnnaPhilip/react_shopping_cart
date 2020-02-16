47d0f4a77e0ee0abc3b722fc94002bb7f84e1428
Sheba <sheba.aphilip@gmail.com>
2020-02-16 20:06:57 +1300

    added styles in Cart for displaying the added items

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -6,19 +6,21 @@ const Cart = ({cartItems}:any) => {
     return (
         <div>
            <h4>Cart is empty</h4> 
-           <div className="cart-column">
-               <label>Product</label>
-               <label>Price</label>
-               <label>Qty</label>
-               <label>Total</label>
+           <div className="cart-label-row">
+               <label className="cproduct-name">Product</label>
+               <label className="cproduct-price">Price</label>
+               <label className="cproduct-quantity">Qty</label>
+               <label className="cproduct-total">Total</label>
+               <label></label>
            </div>
            {Items.length >0 &&
            <div>
                {Items.map(item => 
-                   <div>
-                       <div>{item.name}</div>
-                       <div>{item.price}</div>
-                       <div>{item.count}</div>
+                   <div className="cart-product-row">
+                       <div className="cproduct-name">{item.name}</div>
+                       <div className="cproduct-price">{item.price}</div>
+                       <div className="cproduct-quantity">{item.count}</div>
+                       <div className="cproduct-total">{item.price *item.count}</div>
                    </div>
                )}
            </div>
