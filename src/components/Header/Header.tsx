2ccd31c43b1d0759b71e81c0e4b008decdda846c
Sheba <sheba.aphilip@gmail.com>
2020-02-17 00:18:51 +1300

    added logic for mouse outside click

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './Header.scss'
 import Cart from '../Cart/Cart';
 
@@ -6,14 +6,28 @@ import Cart from '../Cart/Cart';
 const Header =({cartItems, handleRemoveItem}:any) =>{
     const [isCartOpen, toggleCart] = useState(false);
     const handleClickCart = () => toggleCart(!isCartOpen);
+    const myRef:any = useRef();
+    const iconRef:any = useRef();
+
+    const handleClickOutside = (e:Event) => {
+        if (!iconRef.current.contains(e.target) && !myRef.current.contains(e.target)) {
+            toggleCart(false)
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    });
+
     return (
         <div className="container">
             <img src='/assets/logo.png' className="logo" alt="Logo" />
             <div className="cart">
-                <a  href="#">
+                <a  href="#" ref={iconRef}>
                     <img className="icon" src='/assets/carticon.png' alt="Cart" onClick={handleClickCart}/>
                 </a>
-                <div className={isCartOpen?"cart-status open":"cart-status"}>
+                <div ref={myRef} className={isCartOpen?"cart-status open":"cart-status"}>
                     <Cart cartItems={cartItems} handleRemoveItem={handleRemoveItem}/>
                 </div>
             </div>
