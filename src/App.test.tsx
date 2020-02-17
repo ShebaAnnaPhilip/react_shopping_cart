407f1241491c1f61f61f422f2c12025acb732756
Sheba <sheba.aphilip@gmail.com>
2020-02-17 20:34:06 +1300

    added test for App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,21 +1,60 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+import { shallow } from 'enzyme';
+import Header from './components/Header/Header';
+import ProductList from './components/ProductList/ProductList';
+import {products} from './constants/index';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe ('App',() => {
+  let wrapper:any;
 
-test('Shows the Products list header', () => {
-  // Arrange
-  // ...
+  beforeEach(() => {
+    wrapper = shallow(<App/>)
+    wrapper.setState({ cartItems: []});
 
-  // Act
-  const { getByText } = render(<App />);
-  // Assert
-  expect(getByText('Products')).toBeTruthy();
-});
+  });
+
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders Header compoent', () => {
+    expect(wrapper.find(Header)).toHaveLength(1);
+  });
+
+  it('renders ProductList compoent', () => {
+    expect(wrapper.find(ProductList)).toHaveLength(1);
+  });
+
+  it ('should add products to cart', () => {
+    
+    const itemToAdd:any = products[0];
+    let cartItems:any
+    let productAlreadyInCart = false;
+    if(!productAlreadyInCart){
+          cartItems = wrapper.state().cartItems.concat(itemToAdd);
+        }
+    wrapper.setState({ cartItems});
+    expect(wrapper.state().cartItems).toHaveLength(1);
+    
+  })
+
+  it ('should remove products from cart', () => {
+
+    const id = '1';
+    const cartItems:any[] = products;
+    let filteredCart = cartItems.filter(cartitem => cartitem.id !== id)
+    wrapper.setState({cartItems : filteredCart});
+    expect(wrapper.state().cartItems).toHaveLength(8);
+
+  })
+
+})
+
+
+
